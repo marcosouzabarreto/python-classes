14859ff33256e2d0c5e6aeb94fb68d41643a575b
Marco Barreto <marcosouzabarreto@gmail.com>
2021-08-16 22:43:50 -0300

    Tipos de dados

---
+++ Introdução/tipos-de-dados.py
@@ -0,0 +1,20 @@
+# IDE - Ambiente de desenvolvimento integrado
+
+# Tipos de dados
+# --------------
+# Int - 12
+# Float - 10.2
+# Char - '!'
+# String - 'Luan'
+# Bool - true/false
+
+# Operadores
+# --------------
+# + Soma
+# - Subtração
+# * Multiplicação
+# / Divisão
+# // Divisão real
+# % Resto de divisão
+
+
