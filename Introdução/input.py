9d35de58139493f9495f6710390ec8d75df04a2e
Marco Barreto <marcosouzabarreto@gmail.com>
2021-08-16 23:06:07 -0300

    Funcao input

---
+++ Introdução/input.py
@@ -0,0 +1,15 @@
+# Observações
+# ------------
+# \n Pula a linha
+# \t Dá um tab
+
+# pai = input('Digite o nome do seu pai\n')
+# print(f'O nome do seu pai é {pai}')
+
+# O input pega os dados como string
+# A função int() transforma os dados de string para int
+
+numero1 = int(input("Digite um numero\n"))
+numero2 = int(input("Digite outro numero\n"))
+resultado = numero1 + numero2
+print(f"O número {numero1} mais o número {numero2} é igual a {resultado}")
