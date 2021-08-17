e2eaf41adc27fc4e6add605a63304b00a71f773d
Marco Barreto <marcosouzabarreto@gmail.com>
2021-08-16 22:44:27 -0300

    Funcao print

---
+++ Introdução/funcao-print.py
@@ -0,0 +1,9 @@
+# Observações
+# --------------
+# Concatenar é juntar strings com um +
+# print('LU' + 'AN')
+# .format={}
+# print('NINA tem {} anos'.format(idade))
+
+idade = 16
+print(f'Você tem {idade} anos')
